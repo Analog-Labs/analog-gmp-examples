0a64eb5d822b5aee364a8230442cfd1de199c2fc
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2024-04-19 00:51:11 +0100

    Update to latest analog-gmp version (#1)

--- examples/simple/Counter.t.sol
+++ examples/simple/Counter.t.sol
@@ -2,31 +2,71 @@
 pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
+import {IGateway} from "@analog-gmp/interfaces/IGateway.sol";
 import {GmpTestTools} from "@analog-gmp-testing/GmpTestTools.sol";
 import {Counter} from "./Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
+    /**
+     * @dev The address of Alice
+     */
+    address private constant alice = address(bytes20(keccak256("Alice")));
+
+    /**
+     * @dev Sepolia network ID and gateway
+     */
+    uint16 private constant sepoliaId = GmpTestTools.SEPOLIA_NETWORK_ID;
+    IGateway private constant sepoliaGateway = GmpTestTools.SEPOLIA_GATEWAY;
+
+    /**
+     * @dev Shibuya network ID and gateway
+     */
+    uint16 private constant shibuyaId = GmpTestTools.SHIBUYA_NETWORK_ID;
+    IGateway private constant shibuyaGateway = GmpTestTools.SHIBUYA_GATEWAY;
+
+    /**
+     * @dev Test setup, deploys the gateways contracts and creates shibuya and sepolia forks respectively.
+     * The `Counter.sol` contract is deployed on the Sepolia fork.
+     */
     function setUp() external {
+        // Setup test environment, deploy gateways and create forks
         GmpTestTools.setup();
-        GmpTestTools.switchNetwork(GmpTestTools.SEPOLIA_NETWORK_ID);
-        counter = new Counter(address(GmpTestTools.SEPOLIA_GATEWAY));
+
+        // Deploy Counter contract on Sepolia
+        GmpTestTools.switchNetwork(sepoliaId);
+        counter = new Counter(address(sepoliaGateway));
     }
 
+    /**
+     * @dev Example incrementing the counter by sending a message from Shibuya to Sepolia
+     */
     function test_Increment() external {
-        GmpTestTools.switchNetwork(GmpTestTools.SEPOLIA_NETWORK_ID);
-        assertEq(counter.number(), 0);
-        // Deposit
-        bytes32 source = bytes32(uint256(uint160(msg.sender)));
-        GmpTestTools.SEPOLIA_GATEWAY.deposit{value: 100 ether}(source, GmpTestTools.SEPOLIA_NETWORK_ID);
+        // Fund `alice` account with 100 ether in all networks
+        GmpTestTools.deal(alice, 100 ether);
 
-        GmpTestTools.switchNetwork(GmpTestTools.SHIBUYA_NETWORK_ID);
-        GmpTestTools.SHIBUYA_GATEWAY.submitMessage(address(counter), GmpTestTools.SEPOLIA_NETWORK_ID, 100_000, "");
+        // Set alice as `msg.sender` and `tx.origin` of all subsequent calls
+        vm.startPrank(alice, alice);
 
-        GmpTestTools.switchNetwork(GmpTestTools.SEPOLIA_NETWORK_ID);
+        // Switch to Sepolia Fork
+        GmpTestTools.switchNetwork(sepoliaId);
         assertEq(counter.number(), 0);
 
+        // Deposit funds to pay for the execution cost from Shibuya to Sepolia
+        bytes32 source = bytes32(uint256(uint160(alice)));
+        sepoliaGateway.deposit{value: 1 ether}(source, shibuyaId);
+        assertEq(counter.number(), 0);
+
+        // Submit a new GMP from Shibuya to Sepolia
+        GmpTestTools.switchNetwork(shibuyaId);
+        shibuyaGateway.submitMessage(address(counter), sepoliaId, 100_000, "");
+
+        // Check the counter before relaying the GMP message
+        GmpTestTools.switchNetwork(sepoliaId);
+        assertEq(counter.number(), 0);
+
+        // Relay all pending GMP messages and check the counter again
         GmpTestTools.relayMessages();
         assertEq(counter.number(), 1);
     }
